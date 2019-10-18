7e7dc9e110a1750216393e76723f542b88c80530
tuf_unkn0wn <40415158+unkn0wnh4ckr@users.noreply.github.com>
2019-10-18 17:08:05 -0400

    Update boOoterupdate.py

--- boOoterupdate.py
+++ boOoterupdate.py
@@ -1,3 +1,3 @@
 import os
 print "UPDATING..."
-os.system('cd /root/ && rm -fr boOoter && git clone https://github.com/unkn0wnh4ckr/boOoter && echo "[UPDATED]:  Restart Your Terminal
+os.system('cd /root/ && rm -fr boOoter && git clone https://github.com/unkn0wnh4ckr/boOoter && echo "[UPDATED]:  Restart Your Terminal"
